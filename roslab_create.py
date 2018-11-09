c4723d7236ebfc91dbc3d2480a79b81704f0d58d
Enric Cervera <ecervera@uji.es>
2018-11-09 13:15:16 +0100

    catkin build and source dependencies

--- roslab_create.py
+++ roslab_create.py
@@ -4,6 +4,45 @@ import os.path
     
 distros = ['kinetic', 'lunar', 'melodic']
 
+def getDirectoryList(path):
+    directoryList = []
+
+    #return nothing if path is a file
+    if os.path.isfile(path):
+        return []
+
+    #add dir to directorylist if it contains a package.xml
+    if len([f for f in os.listdir(path) if f=='package.xml'])>0:
+        directoryList.append(path)
+
+    for d in os.listdir(path):
+        new_path = os.path.join(path, d)
+        if os.path.isdir(new_path):
+            directoryList += getDirectoryList(new_path)
+
+    return directoryList
+
+def source_commands(list_pack):
+    s = ''
+    for p in list_pack:
+        s += """\nRUN apt-get update \\
+  && apt-get install -yq --no-install-recommends \\"""
+        for d in p['depends']:
+            s += '\n    ' + d + ' \\'
+        s += """
+ && apt-get clean \\
+ && rm -rf /var/lib/apt/lists/*
+"""
+        s += "\nRUN git clone " + p['repo'] + " /" + p['name'] + " \\\n && cd /" + p['name'] + " \\\n"
+        if p['build'] == 'cmake':
+            s += """ && mkdir build \\
+ && cd build \\
+ && cmake ../ \\
+ && make -j4 install \\
+"""
+            s += " && rm -fr /" + p['name'] + "\n"
+    return s
+    
 def main():
     if len(sys.argv) != 2:
         print("Usage: %s <folder>" % sys.argv[0])
@@ -36,11 +75,16 @@ USER root\n
     apt_tail = """ && apt-get clean \\
  && rm -rf /var/lib/apt/lists/*
 """
-    if os.path.isfile("package.xml"):
+    #if os.path.isfile("package.xml"):
+    if getDirectoryList("."):
         mid_section = "\nRUN mkdir -p ${HOME}/catkin_ws/src/" + yl['name'] + \
             "\nCOPY . ${HOME}/catkin_ws/src/" + yl['name'] + "/." + """
 RUN cd ${HOME}/catkin_ws \\
- && /bin/bash -c "source /opt/ros/DISTRO/setup.bash && catkin_make"
+ && apt-get update \\
+ && /bin/bash -c "source /opt/ros/DISTRO/setup.bash && rosdep update && rosdep install --as-root apt:false --from-paths src --ignore-src -r -y" \\
+ && apt-get clean \\
+ && rm -rf /var/lib/apt/lists/* \\
+ && /bin/bash -c "source /opt/ros/DISTRO/setup.bash && catkin build"
 
 RUN echo "source ~/catkin_ws/devel/setup.bash" >> ${HOME}/.bashrc\n
 """
@@ -58,10 +102,13 @@ WORKDIR ${HOME}
 
     with open("Dockerfile", "w") as dockerfile:
         dockerfile.write(global_head)
-        dockerfile.write(apt_head)
-        for p in yl['packages']:
-            dockerfile.write("    %s \\\n" % p)
-        dockerfile.write(apt_tail)
+        if 'packages' in yl.keys():
+            dockerfile.write(apt_head)
+            for p in yl['packages']:
+                dockerfile.write("    %s \\\n" % p)
+            dockerfile.write(apt_tail)
+        if 'source' in yl.keys():
+            dockerfile.write(source_commands(yl['source']))
         dockerfile.write(mid_section)
         dockerfile.write(global_tail)
     
