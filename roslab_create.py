8be5f1845fc0da9b80a650a41ed6570ba3ee0d2b
Enric Cervera <ecervera@uji.es>
2018-12-07 10:48:50 +0100

    fix env flag

--- roslab_create.py
+++ roslab_create.py
@@ -197,7 +197,7 @@ docker run --rm \\
     --env="DISPLAY" \\
     --env="QT_X11_NO_MITSHM=1" \\
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \\
-    -env="XAUTHORITY=$XAUTH" \\
+    --env="XAUTHORITY=$XAUTH" \\
     --volume="$XAUTH:$XAUTH" \\
     --runtime=nvidia \\
 """)
