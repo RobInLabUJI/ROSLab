949d8f913c982816555691e5fa09f249a3e80d78
Enric Cervera <ecervera@uji.es>
2019-02-25 20:38:06 +0100

    add cudnn 6

--- components/cudnn.py
+++ components/cudnn.py
@@ -1,6 +1,6 @@
 import os, sys
 
-versions = ['5-runtime', '5-devel', '7-runtime', '7-devel']
+versions = ['5-runtime', '5-devel', '6-runtime', '6-devel', '7-runtime', '7-devel']
 
 DOCKER_CUDNN_HEADER = """
 ###################################### CUDNN ###################################
@@ -20,6 +20,17 @@ LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
 RUN apt-get update && apt-get install -y --no-install-recommends \\
             libcudnn7=$CUDNN_VERSION-1+cuda10.0 && \\
     apt-mark hold libcudnn7 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
+DOCKER_RUNTIME_CONTENTS['16.04']['8.0']['6'] = """
+RUN echo "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
+
+ENV CUDNN_VERSION 6.0.21
+LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+            libcudnn6=$CUDNN_VERSION-1+cuda8.0 && \
     rm -rf /var/lib/apt/lists/*
 """
 
@@ -49,6 +60,18 @@ RUN apt-get update && apt-get install -y --no-install-recommends \\
             libcudnn7=$CUDNN_VERSION-1+cuda10.0 \\
             libcudnn7-dev=$CUDNN_VERSION-1+cuda10.0 && \\
     apt-mark hold libcudnn7 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
+DOCKER_DEVEL_CONTENTS['16.04']['8.0']['6'] = """
+RUN echo "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
+
+ENV CUDNN_VERSION 6.0.21
+LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+            libcudnn6=$CUDNN_VERSION-1+cuda8.0 \
+            libcudnn6-dev=$CUDNN_VERSION-1+cuda8.0 && \
     rm -rf /var/lib/apt/lists/*
 """
 
