01b724199e4c75cf8c5f12ca8b996fb5c5f20f52
Enric Cervera <ecervera@uji.es>
2019-02-26 07:36:15 +0100

    add more versions

--- components/cuda.py
+++ components/cuda.py
@@ -1,6 +1,6 @@
 import os, sys
 
-versions = ['8.0-runtime', '8.0-devel', '10.0-runtime', '10.0-devel']
+versions = ['8.0-runtime', '8.0-devel', '9.0-runtime', '9.0-devel', '10.0-runtime', '10.0-devel']
 
 DOCKER_CUDA_HEADER = """
 ###################################### CUDA ####################################
@@ -83,6 +83,50 @@ RUN apt-get update && apt-get install -y --no-install-recommends \\
     rm -rf /var/lib/apt/lists/*
 """
 
+DOCKER_RUNTIME_CONTENTS['16.04']['9.0'] = """
+RUN apt-get update && apt-get install -y --no-install-recommends ca-certificates apt-transport-https gnupg-curl && \\
+    rm -rf /var/lib/apt/lists/* && \\
+    NVIDIA_GPGKEY_SUM=d1be581509378368edeec8c1eb2958702feedf3bc3d17011adbf24efacce4ab5 && \\
+    NVIDIA_GPGKEY_FPR=ae09fe4bbd223a84b2ccfce3f60f4b3d7fa2af80 && \\
+    apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub && \\
+    apt-key adv --export --no-emit-version -a $NVIDIA_GPGKEY_FPR | tail -n +5 > cudasign.pub && \\
+    echo "$NVIDIA_GPGKEY_SUM  cudasign.pub" | sha256sum -c --strict - && rm cudasign.pub && \\
+    echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list && \\
+    echo "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
+
+ENV CUDA_VERSION 9.0.176
+
+ENV CUDA_PKG_VERSION 9-0=$CUDA_VERSION-1
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+        cuda-cudart-$CUDA_PKG_VERSION && \\
+    ln -s cuda-9.0 /usr/local/cuda && \\
+    rm -rf /var/lib/apt/lists/*
+
+# nvidia-docker 1.0
+LABEL com.nvidia.volumes.needed="nvidia_driver"
+LABEL com.nvidia.cuda.version="${CUDA_VERSION}"
+
+RUN echo "/usr/local/nvidia/lib" >> /etc/ld.so.conf.d/nvidia.conf && \\
+    echo "/usr/local/nvidia/lib64" >> /etc/ld.so.conf.d/nvidia.conf
+
+ENV PATH /usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}
+ENV LD_LIBRARY_PATH /usr/local/nvidia/lib:/usr/local/nvidia/lib64
+
+# nvidia-container-runtime
+ENV NVIDIA_VISIBLE_DEVICES all
+ENV NVIDIA_DRIVER_CAPABILITIES compute,utility
+ENV NVIDIA_REQUIRE_CUDA "cuda>=9.0"
+
+ENV NCCL_VERSION 2.4.2
+
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+        cuda-libraries-$CUDA_PKG_VERSION \\
+        cuda-cublas-9-0=9.0.176.4-1 \\
+        libnccl2=$NCCL_VERSION-1+cuda9.0 && \\
+    apt-mark hold libnccl2 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
 DOCKER_RUNTIME_CONTENTS['16.04']['8.0'] = """
 RUN apt-get update && apt-get install -y --no-install-recommends ca-certificates apt-transport-https gnupg-curl && \\
     rm -rf /var/lib/apt/lists/* && \\
@@ -153,6 +197,20 @@ RUN apt-get update && apt-get install -y --no-install-recommends \\
 ENV LIBRARY_PATH /usr/local/cuda/lib64/stubs
 """
 
+DOCKER_DEVEL_CONTENTS['16.04']['9.0'] = """
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+        cuda-libraries-dev-$CUDA_PKG_VERSION \\
+        cuda-nvml-dev-$CUDA_PKG_VERSION \\
+        cuda-minimal-build-$CUDA_PKG_VERSION \\
+        cuda-command-line-tools-$CUDA_PKG_VERSION \\
+        cuda-core-9-0=9.0.176.3-1 \\
+        cuda-cublas-dev-9-0=9.0.176.4-1 \\
+        libnccl-dev=$NCCL_VERSION-1+cuda9.0 && \\
+    rm -rf /var/lib/apt/lists/*
+
+ENV LIBRARY_PATH /usr/local/cuda/lib64/stubs
+"""
+
 DOCKER_DEVEL_CONTENTS['16.04']['8.0'] = """
 RUN apt-get update && apt-get install -y --no-install-recommends \\
         cuda-core-$CUDA_PKG_VERSION \\
@@ -176,11 +234,6 @@ ENV LIBRARY_PATH /usr/local/cuda/lib64/stubs
 
 def write(DOCKER_FILE, version, ubuntu):
     if version in versions:
-        #if ubuntu == 18.04 or ubuntu == 'melodic':
-        #    ubuntu = '18.04'
-        #elif ubuntu == 16.04 or ubuntu == 'xenial':
-        #    ubuntu = '16.04'
-
         with open(DOCKER_FILE, "a") as dockerfile:
             dockerfile.write(DOCKER_CUDA_HEADER)
             cuda = version.split('-')[0]
--- components/cudnn.py
+++ components/cudnn.py
@@ -10,6 +10,8 @@ DOCKER_RUNTIME_CONTENTS = {}
 DOCKER_RUNTIME_CONTENTS['18.04'] = {}
 DOCKER_RUNTIME_CONTENTS['16.04'] = {}
 DOCKER_RUNTIME_CONTENTS['18.04']['10.0'] = {}
+DOCKER_RUNTIME_CONTENTS['16.04']['10.0'] = {}
+DOCKER_RUNTIME_CONTENTS['16.04']['9.0'] = {}
 DOCKER_RUNTIME_CONTENTS['16.04']['8.0'] = {}
 
 DOCKER_RUNTIME_CONTENTS['18.04']['10.0']['7'] = """
@@ -19,6 +21,35 @@ LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
 
 RUN apt-get update && apt-get install -y --no-install-recommends \\
             libcudnn7=$CUDNN_VERSION-1+cuda10.0 && \\
+    apt-mark hold libcudnn7 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
+DOCKER_RUNTIME_CONTENTS['16.04']['10.0']['7'] = """
+ENV CUDNN_VERSION 7.4.2.24
+LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
+
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+            libcudnn7=$CUDNN_VERSION-1+cuda10.0 && \\
+    apt-mark hold libcudnn7 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
+DOCKER_RUNTIME_CONTENTS['16.04']['9.0']['7'] = """
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+            libcudnn7=$CUDNN_VERSION-1+cuda9.0 && \\
+    apt-mark hold libcudnn7 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
+DOCKER_RUNTIME_CONTENTS['16.04']['8.0']['7'] = """
+RUN echo "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
+
+ENV CUDNN_VERSION 7.2.1.38
+LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
+
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+            libcudnn7=$CUDNN_VERSION-1+cuda8.0 && \\
     apt-mark hold libcudnn7 && \\
     rm -rf /var/lib/apt/lists/*
 """
@@ -49,6 +80,8 @@ DOCKER_DEVEL_CONTENTS = {}
 DOCKER_DEVEL_CONTENTS['18.04'] = {}
 DOCKER_DEVEL_CONTENTS['16.04'] = {}
 DOCKER_DEVEL_CONTENTS['18.04']['10.0'] = {}
+DOCKER_DEVEL_CONTENTS['16.04']['10.0'] = {}
+DOCKER_DEVEL_CONTENTS['16.04']['9.0'] = {}
 DOCKER_DEVEL_CONTENTS['16.04']['8.0'] = {}
 
 DOCKER_DEVEL_CONTENTS['18.04']['10.0']['7'] = """
@@ -59,6 +92,41 @@ LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
 RUN apt-get update && apt-get install -y --no-install-recommends \\
             libcudnn7=$CUDNN_VERSION-1+cuda10.0 \\
             libcudnn7-dev=$CUDNN_VERSION-1+cuda10.0 && \\
+    apt-mark hold libcudnn7 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
+DOCKER_DEVEL_CONTENTS['16.04']['10.0']['7'] = """
+ENV CUDNN_VERSION 7.4.1.5
+LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
+
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+            libcudnn7=$CUDNN_VERSION-1+cuda10.0 \\
+            libcudnn7-dev=$CUDNN_VERSION-1+cuda10.0 && \\
+    apt-mark hold libcudnn7 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
+DOCKER_DEVEL_CONTENTS['16.04']['9.0']['7'] = """
+ENV CUDNN_VERSION 7.4.2.24
+LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
+
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+            libcudnn7=$CUDNN_VERSION-1+cuda9.0 \\
+            libcudnn7-dev=$CUDNN_VERSION-1+cuda9.0 && \\
+    apt-mark hold libcudnn7 && \\
+    rm -rf /var/lib/apt/lists/*
+"""
+
+DOCKER_DEVEL_CONTENTS['16.04']['8.0']['7'] = """
+RUN echo "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
+
+ENV CUDNN_VERSION 7.2.1.38
+LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
+
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+            libcudnn7=$CUDNN_VERSION-1+cuda8.0 \\
+            libcudnn7-dev=$CUDNN_VERSION-1+cuda8.0 && \\
     apt-mark hold libcudnn7 && \\
     rm -rf /var/lib/apt/lists/*
 """
